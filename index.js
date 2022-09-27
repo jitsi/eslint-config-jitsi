bdc1309df41a833b75dd8e705190ff69116aa2e1
Saúl Ibarra Corretgé <s@saghul.net>
2022-09-27 09:36:48 +0200

    feat: add no-nested-ternary rule
    Ref: https://eslint.org/docs/latest/rules/no-nested-ternary

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ module.exports = {
         'no-invalid-regexp': 2,
         'no-irregular-whitespace': 2,
         'no-negated-in-lhs': 2,
+        'no-nested-ternary': 2,
         'no-obj-calls': 2,
         'no-prototype-builtins': 0,
         'no-regex-spaces': 2,
