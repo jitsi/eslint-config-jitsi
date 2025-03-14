088a1bf6b24d5cc0fa98c0790bf08cb0adbb3fe3
Saúl Ibarra Corretgé <s@saghul.net>
2025-03-14 15:58:06 +0100

    Don't enable jsdoc rules by default

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ module.exports = {
         'es6': true,
         'jasmine': true
     },
-    extends: [
-        '@jitsi/eslint-config/js',
-        '@jitsi/eslint-config/typescript',
-        '@jitsi/eslint-config/jsdoc'
-    ],
     globals: {
         // The globals that (1) are accessed but not defined within many of our
         // files, (2) are certainly defined, and (3) we would like to use
         // without explicitly specifying them (using a comment) inside of our
         // files.
         '__filename': false
-    }
+    },
+    overrides: [
+        {
+            files: [ '*.js' ],
+            extends: [
+                '@jitsi/eslint-config/js'
+            ],
+        },
+        {
+            files: [ '*.ts', '*.tsx' ],
+            extends: [
+                '@jitsi/eslint-config/typescript'
+            ],
+        }
+    ]
 };
